9f15b6d9b7398194230a466327068f876a06e0e7
octofoxx <devilmaycry87x@gmail.com>
2023-05-20 13:07:05 -0700

    added in variables, array for questions and timer. Removed placeholder question section in html. gave functionality to start button to run timer and display/hide elements.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,57 @@
-var startGame = document.querySelector("#startGame");
+var startGame = document.getElementById("startGame");
+var questionsArea = document.getElementById("questionsArea");
+var scores = document.getElementById("scores");
+var timer =document.getElementById("timer");
+
+var questions= [
+   {
+    title: "A true/false value is known as a____ ",
+    answers: ["string","boolean","null","number"],
+    correct: "boolean"
+   },
+   
+   {
+    title: "What is a function?",
+    answers: ["A reuseable block of code","A way to store data while working in the page","A way to get user feedback when they take an action","A way to compare equality and type of values"],
+    correct: "A reuseable block of code"
+   },
+
+   {
+    title: "What is 'API' an acronym for?",
+    answers: ["Average Processing Interval","Annual Programmer's Invitational","Application Programming Interface","Accounted Per Index"],
+    correct: "Application Programming Interface"
+   }
+];
+
+var gameStarted = false;
+var countdown = 100;
 
 var logCheck = function(){
     console.log ("Hello, World")
 }
 
-startGame.addEventListener("click", logCheck);
+startGame.addEventListener("click", function() {
+    questionsArea.style.display = "block";
+    startGame.style.display = "none";
+    timer.style.display = "flex", "justify-content: center";
+    gameStarted =true;
+
+    var interval = setInterval(function() {
+        if (!gameStarted) {
+            return;
+        }
+        countdown --;
+
+        if (countdown <= 0) {
+            clearInterval(interval);
+
+        }
+    
+        showCountdownToPage();
+    }, 1000);
+
+});
+
+function showCountdownToPage() {
+    timer.textContent = "Time Remaining " + countdown;
+}
